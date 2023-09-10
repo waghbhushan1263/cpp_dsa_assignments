5ce242f25af650fb8ce89be77b524d8ab84b5f7c
waghbhushan1263 <136261341+waghbhushan1263@users.noreply.github.com>
2023-09-10 11:15:39 +0530

    Add files via upload

---
+++ assign_1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"example no 1";
+    cout<<endl;
+    cout<<"physics"<<endl;
+    cout<<"wallah"<<endl;
+    cout<<endl;
+    cout<<"example no 2";
+    cout<<endl;
+    cout<<5+5;
+    cout<<endl;
+    cout<<"example no 3";
+    cout<<endl;
+    cout<<"int need 4 bytes";
+    cout<<"float need 4 bytes";
+    cout<<"bool need 1 bytes";
+    cout<<endl;
+    cout<<"example no 4";
+    cout<<endl;
+    cout<<"output is 6 4";
+    cout<<endl;
+    cout<<"example no 5";
+    cout<<endl;
+    float area=3.14*10*10;
+    cout<<"area is:"<<area;
+    cout<<endl;
+    cout<<"example no 6";
+    cout<<endl;
+    cout<<2;
+    cout<<endl;
+}
